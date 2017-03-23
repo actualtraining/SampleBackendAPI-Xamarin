e6cb275f89cba191bfd437b57bba8b3cf6d8b771
erick <erick@DESKTOP-JIKBK0G>
2017-03-23 15:08:36 +0700

    menambakan manual ado

--- DAL/KategoriDAL.cs
+++ DAL/KategoriDAL.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 using System.Data.SqlClient;
 using System.Configuration;
+using BO;
 
 namespace DAL
 {
     public class KategoriDAL : IDisposable
     {
+        public string GetConnStr()
+        {
+            return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
 
+        public IEnumerable<Kategori> GetAllKategori()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                List<Kategori> listKategori = new List<Kategori>();
+                string strSql = @"select * from Kategori order by NamaKategori asc";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        listKategori.Add(new Kategori
+                        {
+                            KategoriID = Convert.ToInt32(dr["KategoriID"]),
+                            NamaKategori = dr["NamaKategori"].ToString()
+                        });
+                    }
+                }
+
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
 
         public void Dispose()
         {
