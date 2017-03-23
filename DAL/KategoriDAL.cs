5199b846061ad331b77d2305a17886e54b7b7f00
erick <erick@DESKTOP-JIKBK0G>
2017-03-23 15:24:05 +0700

    menambahkan method

--- DAL/KategoriDAL.cs
+++ DAL/KategoriDAL.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Configuration;
 using BO;
+using Dapper;
 
 namespace DAL
 {
@@ -17,33 +18,52 @@ namespace DAL
             return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         }
 
+        //public IEnumerable<Kategori> GetAllKategori()
+        //{
+        //    using (SqlConnection conn = new SqlConnection(GetConnStr()))
+        //    {
+        //        List<Kategori> listKategori = new List<Kategori>();
+        //        string strSql = @"select * from Kategori order by NamaKategori asc";
+        //        SqlCommand cmd = new SqlCommand(strSql, conn);
+                
+        //        conn.Open();
+        //        SqlDataReader dr = cmd.ExecuteReader();
+        //        if(dr.HasRows)
+        //        {
+        //            while (dr.Read())
+        //            {
+        //                listKategori.Add(new Kategori
+        //                {
+        //                    KategoriID = Convert.ToInt32(dr["KategoriID"]),
+        //                    NamaKategori = dr["NamaKategori"].ToString()
+        //                });
+        //            }
+        //        }
+
+        //        dr.Close();
+        //        cmd.Dispose();
+        //        conn.Close();
+
+        //        return listKategori;
+        //    }
+        //}
+
         public IEnumerable<Kategori> GetAllKategori()
         {
             using (SqlConnection conn = new SqlConnection(GetConnStr()))
             {
-                List<Kategori> listKategori = new List<Kategori>();
                 string strSql = @"select * from Kategori order by NamaKategori asc";
-                SqlCommand cmd = new SqlCommand(strSql, conn);
+                return conn.Query<Kategori>(strSql);
+            }
+        }
 
-                conn.Open();
-                SqlDataReader dr = cmd.ExecuteReader();
-                if(dr.HasRows)
-                {
-                    while (dr.Read())
-                    {
-                        listKategori.Add(new Kategori
-                        {
-                            KategoriID = Convert.ToInt32(dr["KategoriID"]),
-                            NamaKategori = dr["NamaKategori"].ToString()
-                        });
-                    }
-                }
-
-                dr.Close();
-                cmd.Dispose();
-                conn.Close();
-
-                return listKategori;
+        public Kategori GetKategoriByID(int kategoriID)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Kategori where KategoriID=@KategoriID";
+                var param = new { KategoriID = kategoriID };
+                return conn.QuerySingle<Kategori>(strSql, param);
             }
         }
 
